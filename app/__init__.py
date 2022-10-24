417f20565c481ced0c00ad896f5e90b9d8d70e75
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-10-24 09:09:48 -0700

    fixed typo which caused a TypeError message on line 46

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,5 @@ def create_app(test_config=None):
     app = Flask(__name__)
     from .routes import  planets_bp
     app.register_blueprint(planets_bp)
-
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
@@ -43,7 +44,7 @@ def validate_planet(planet_id):
     
     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
 
-@planets_bp.route("/<planet_id>", method= ["GET"])
+@planets_bp.route("/<planet_id>", methods= ["GET"])
 def handle_planet(planet_id):
     planet = validate_planet(planet_id)
     return planet
