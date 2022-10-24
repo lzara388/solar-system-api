103b69d5c6f836192b7417cd539d08c8e606dde7
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-10-24 09:03:21 -0700

    completed wave 2: created new route to grab one planet based on ID, and the also created a helper function to check if ID is valid, else will return appropriate error message

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, from_sun):
@@ -18,14 +18,36 @@ planets_bp = Blueprint('planets_bp', __name__, url_prefix= "/planets")
 
 @planets_bp.route("", methods=["GET"])
 def handle_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "from_sun" : planet.from_sun
-            }
-        )
+    # planets_response = []
+    # for planet in planets:
+    #     planets_response.append(
+    #         {
+    #             "id": planet.id,
+    #             "name": planet.name,
+    #             "description": planet.description,
+    #             "from_sun" : planet.from_sun
+    #         }
+    #     )
+    planets_response = [vars(planet) for planet in planets]
     return jsonify(planets_response)
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return vars(planet)
+    
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+@planets_bp.route("/<planet_id>", method= ["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return planet
+
+
+
+
